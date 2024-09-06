89746e69dc8e18a951bf7006ee23d540a1d9cf48
Wwbbbbb <267057047@qq.com>
2024-09-06 18:55:40 +0800

    refactor(HX711): 使用模板重构权重配置和读取
    使用模板函数重构了`storeWeightConfiToEEPROM`和`readWeightConfiFromEEPROM`，以提高代码可重用性并清理`HX711.cpp`中的EEPROM操作。在`HX711.h`中添加了新的按钮定义和重量读取方法，以简化外部接口。此外，通过从`main.cpp`中移除未使用的变量并添加设置代码，优化了重量配置过程。
    
    通过这些更改，代码变得更加模块化，允许在HX711称重传感器的配置和读取过程中更灵活地使用EEPROM存储。此外，这些修改增强了代码的可读性和可维护性。

--- include/HX711.h
+++ include/HX711.h
@@ -6,10 +6,22 @@
 #define HX711_SCK D7
 #define HX711_DT D8
 
-#define OFFSET_VAL 862 //ADC 偏移值
+#define ZERO_BUTTON D6
+#define CHECK_MINUS_BUTTON D5
+#define CHECK_PLUS_BUTTON D3
+
+//unsigned int OFFSET_VAL = 862; //ADC 偏移值
+
 void Init_Hx711();
 unsigned long HX711_Read(void);
-unsigned long Get_Weight(unsigned long Weight_Maopi);
-unsigned long Get_Maopi(unsigned long Weight_Maopi);
+unsigned long Get_Weight();
+void Get_Maopi();
+void weightConfig();
+
+template<typename valueType>
+void storeWeightConfiToEEPROM(int addr, valueType val);
+
+template<typename valueType>
+valueType readWeightConfiFromEEPROM(int addr);
 
 #endif
--- src/HX711.cpp
+++ src/HX711.cpp
@@ -1,7 +1,11 @@
 #include "hx711.h"
+#include "EEPROM.h"
 
-int GapValue = 213;
+int GapValue = 213; 
+unsigned long Weight_Maopi = 0;
 
+int MAOPI_ADDR = 1000;//防止与wificonfig冲突
+int CHECK_ADDR = 1010;
 //****************************************************
 //初始化HX711
 //****************************************************
@@ -9,33 +13,66 @@ void Init_Hx711()
 {
 	pinMode(HX711_SCK, OUTPUT);
 	pinMode(HX711_DT, INPUT);
+	if(readWeightConfiFromEEPROM<char>(CHECK_ADDR) != 0xFF){
+		GapValue = readWeightConfiFromEEPROM<int>(CHECK_ADDR);
+		Weight_Maopi =  readWeightConfiFromEEPROM<unsigned long>(MAOPI_ADDR);
+	}
+	else{
+		storeWeightConfiToEEPROM(MAOPI_ADDR,(unsigned long) 8573096);
+		storeWeightConfiToEEPROM(CHECK_ADDR,(int)213);
+	}
+	Serial.print("EEPROM毛皮重量:");
+	Serial.println(Weight_Maopi);
+	Serial.print("EEPROM校准值:");
+	Serial.println(GapValue);
 }
 
 
 //****************************************************
 //获取毛皮重量
 //****************************************************
-unsigned long Get_Maopi(unsigned long Weight_Maopi)
+void Get_Maopi()
 {   
-    if(digitalRead(D6) == LOW){
-        delay(20);
-        if(digitalRead(D6) == LOW){
-            return HX711_Read();
+    if(digitalRead(ZERO_BUTTON) == LOW){
+        delay(50);
+        if(digitalRead(ZERO_BUTTON) == LOW){
+            Weight_Maopi = HX711_Read();
+			storeWeightConfiToEEPROM(MAOPI_ADDR,Weight_Maopi);
+			Serial.print("毛皮重量已记录:");
+			Serial.println(Weight_Maopi);
         }
     }
-	return Weight_Maopi;		
-} 
+	//return Weight_Maopi;		
+}
+
+void modifyGapValue(){
+    if(digitalRead(CHECK_MINUS_BUTTON) == LOW){
+        delay(50);
+        if(digitalRead(CHECK_MINUS_BUTTON) == LOW){
+			GapValue = --GapValue>0? GapValue : 1;
+			storeWeightConfiToEEPROM(CHECK_ADDR,GapValue);
+			Serial.print("GapValue has been modified:");
+			Serial.println(GapValue);
+        }
+    }
+	if(digitalRead(CHECK_PLUS_BUTTON) == LOW){
+        delay(50);
+        if(digitalRead(CHECK_PLUS_BUTTON) == LOW){
+			GapValue++;
+			storeWeightConfiToEEPROM(CHECK_ADDR,GapValue);
+			Serial.print("GapValue has been modified:");
+			Serial.println(GapValue);
+        }
+    }
+}
 
 //****************************************************
 //称重
 //****************************************************
-unsigned long Get_Weight(unsigned long Weight_Maopi)
+unsigned long Get_Weight()
 {
 	unsigned long Weight_Shiwu = HX711_Read();
-    //unsigned long Weight_Chen = HX711_Read();
-	return Weight_Shiwu > Weight_Maopi? (Weight_Shiwu - Weight_Maopi)/GapValue : 0;				//获取实物的克重
-	//Weight_Shiwu = ((Weight_Shiwu/GapValue)); //- OFFSET_VAL); 	
-	//return Weight_Shiwu; //显示克重
+	return Weight_Shiwu > Weight_Maopi? (Weight_Shiwu - Weight_Maopi)/GapValue : 0;				//获取实物的克
 }
 
 //****************************************************
@@ -70,4 +107,34 @@ unsigned long HX711_Read(void)	//增益128
 	delayMicroseconds(1);
 	
 	return(count);
+}
+
+template<typename valueType>
+void storeWeightConfiToEEPROM(int addr, valueType val){
+    if (addr + sizeof(valueType) > EEPROM.length()) {
+        Serial.println("EEPROM write out of range");
+        return;
+    }	
+	for(unsigned int i = 0; i < sizeof(val); i++){
+		EEPROM.write(addr + i, (val >> (i * 8)) & 0xFF);
+	}
+	EEPROM.commit();
+}
+
+template<typename valueType>
+valueType readWeightConfiFromEEPROM(int addr){
+    if (addr + sizeof(valueType) > EEPROM.length()) {
+        Serial.println("EEPROM write out of range");
+        return 0;
+    }
+    valueType val = 0;
+    for (unsigned int i = 0; i < sizeof(val); i++) {
+        val |= (valueType)(EEPROM.read(addr + i)) << (i * 8);
+    }
+	return val;	
+}
+
+void weightConfig(){
+	Get_Maopi();
+	modifyGapValue();
 }
