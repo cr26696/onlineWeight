3d6846fd23b36cbffd980d516bbc5d0236e63c61
cr26696 <2669648220@qq.com>
2024-09-07 16:42:27 +0800

    初始化段显示时使用占位符'BBBBBBBB'代替'00000000'，以示未开始显示数值。同时，扩展了SegCharToNum函数，正确映射字符'5'到数字5，并增加了对字符'-'、'E'、'H'、'L'和'P'的处理，分别映射到10到14，其他字符统一映射到15。

--- src/SegDisplay.cpp
+++ src/SegDisplay.cpp
@@ -3,7 +3,7 @@
 void SegDisplayinit()
 {
   Init_MAX7219();
-  SegWrite("full","00000000");
+  SegWrite("full","BBBBBBBB");
 }
 
 
@@ -13,12 +13,17 @@ uint8_t SegCharToNum(char c){
   else if(c=='2')return 2;
   else if(c=='3')return 3;
   else if(c=='4')return 4;
-  else if(c=='5')return 6;
+  else if(c=='5')return 5;
   else if(c=='6')return 6;
   else if(c=='7')return 7;
   else if(c=='8')return 8;
   else if(c=='9')return 9;
-  else return 10;
+  else if(c=='-')return 10;
+  else if(c=='E')return 11;
+  else if(c=='H')return 12;
+  else if(c=='L')return 13;
+  else if(c=='P')return 14;
+  else return 15;
 }
 /**
  * 向数码管写数据
