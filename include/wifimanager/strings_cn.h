35a1ac053e68993cc18665b2cfff277cea4f8afb
cr26696 <2669648220@qq.com>
2024-09-09 18:44:29 +0800

    WiFimanager库本地化

---
+++ include/wifimanager/strings_cn.h
@@ -0,0 +1,16 @@
+/**
+ * Contents of this file have moved to 2 new locations
+ * wm_strings_nn.h
+ *  wm_consts_nn.h
+ * @author winlam
+ */
+
+#warning "This file is deprecated"
+
+#ifndef _STRINGS_CN_H_
+#define _STRINGS_CN_H_
+
+// strings files must include a consts file!
+#include "wm_strings_cn.h" // include constants, tokens, routes
+
+#endif
